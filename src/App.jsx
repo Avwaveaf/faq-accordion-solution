d613d118fa058f76ecc847333cb8b8d296398197
Afif <49422146+Avwaveaf@users.noreply.github.com>
2024-01-30 14:20:18 +0700

    fdsf

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ function App() {
       <section className="h-screen w-screen flex flex-col justify-center items-center relative">
         {/* Large screen background */}
         <div
-          style={{ backgroundImage: `url(/images/background-pattern-desktop.svg})` }}
-          className="hidden md:block w-full h-1/3 absolute -z-10 top-0 bg-cover bg-[url(/images/background-pattern-desktop.svg)]"
+          style={{ backgroundImage: `url(/images/background-pattern-desktop.svg)` }}
+
+          className="hidden md:block w-full h-1/3 absolute -z-10 top-0 bg-cover "
           aria-hidden="true"
         ></div>
 
